4e0e70abddaeeb9df3b9dd5d064f4c46b08ffae1
ElisabethFox <lisa.black.fox17@gmail.com>
2023-04-09 21:45:40 +0300

    start dev

---
+++ memory_game.js
@@ -0,0 +1,57 @@
+const cards = document.querySelectorAll('.memory-card');
+
+let hasFlippedCard = false;
+let boardLocked = false;
+let firstCard, secondCard;
+
+
+const flipCard = (event) => {
+    if (boardLocked) return;
+
+    const target = event.target.parentElement;
+
+    if (target === firstCard) return;
+
+    target.classList.add('flip');
+
+    if (!hasFlippedCard) {
+        hasFlippedCard = true;
+        firstCard = target;
+    } else {
+        hasFlippedCard = false;
+        secondCard = target;
+        checkForMatch();
+    }
+};
+
+const checkForMatch = () => {
+    const isEqual = firstCard.dataset.framework === secondCard.dataset.framework;
+    isEqual ? disabledCards() : unflipCards();
+};
+
+const disabledCards = () => {
+    firstCard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click', flipCard);
+};
+
+const unflipCards = () => {
+    boardLocked = true;
+
+    setTimeout(() => {
+    firstCard.classList.remove('flip');
+    secondCard.classList.remove('flip');
+    resetBoard();
+    }, 1000);
+};
+
+const resetBoard = () => {
+    hasFlippedCard = boardLocked = false;
+    firstCard = secondCard = null;
+};
+
+cards.forEach((card) => {
+    card.addEventListener('click', flipCard);
+
+    const randomIndex = Math.floor(Math.random() * cards.length);
+    card.style.order = randomIndex;
+});
